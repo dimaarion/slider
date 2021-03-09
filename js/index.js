37af1ae23071a69eceb4245225a09aa85db40c9e
Dmitriy <48031182+dimaarion@users.noreply.github.com>
2021-03-10 01:14:24 +0200

    dimaarion/slider.git

--- js/index.js
+++ js/index.js
@@ -1,42 +1,72 @@
-function slider() {
-	const img = {
-		slider:document.getElementById('slider'),
-		childEl(a,b){
-          return  a.appendChild(document.body.appendChild(document.createElement(b)))
-		},
-		createArr(n){
-            let a = [];
-            for (let i = 0; i < n; i++) {
-                 a[i] = i;
-            }
-            return a;
-	}
-}
+const slider = {
+  id: "slider",
+  idUl: "ulImg",
+  classLi: "liImg",
+  liStyle: {
+    listStyle: "none"
+  },
+  getId(e) {
+    return document.getElementById(e);
+  },
+  getClass(e) {
+    return Array.from(document.getElementsByClassName(e));
+  },
+  css(e, o = {}) {
+    let t = "";
+    let rezult = [];
+    let ob = "";
+    Object.values(o).map(function (x, i) {
+      ob = Object.keys(o)[i].match(/[A-Z]/);
+      if (ob) {
+        t = "-" + Object.keys(o)[i].match(/[A-Z]/).join("").toLowerCase();
+      } else {
+        t = "";
+      }
 
-let div = img.childEl(img.slider,'div');
-let div2 = img.childEl(div,'div');
-let countImg = img.createArr(10);
-countImg.map((x)=>img.childEl(img.childEl(div2,'div'),'div'))
-.map((x2,j)=>{x2.style.backgroundImage = "url(/img/1.jpg)";x2.className = "images";})
-.map((x3,f)=>console.log(document.getElementsByClassName('images')[f].offsetParent.className = "imagesBlockHidden"));
-div2.className = "imageBlock";
-document.getElementsByClassName('imagesBlockHidden')[1].style.marginLeft = '100px';
-document.getElementsByClassName('images')[1].style.marginLeft = '-100px';
-document.getElementsByClassName('imagesBlockHidden')[2].style.marginLeft = '200px';
-document.getElementsByClassName('images')[2].style.marginLeft = '-200px';
-document.getElementsByClassName('imagesBlockHidden')[3].style.marginLeft = '300px';
-document.getElementsByClassName('images')[3].style.marginLeft = '-300px';
-document.getElementsByClassName('imagesBlockHidden')[4].style.marginLeft = '400px';
-document.getElementsByClassName('images')[4].style.marginLeft = '-400px';
-document.getElementsByClassName('imagesBlockHidden')[5].style.marginLeft = '500px';
-document.getElementsByClassName('images')[5].style.marginLeft = '-500px';
-document.getElementsByClassName('imagesBlockHidden')[6].style.marginLeft = '600px';
-document.getElementsByClassName('images')[6].style.marginLeft = '-600px';
-document.getElementsByClassName('imagesBlockHidden')[7].style.marginLeft = '700px';
-document.getElementsByClassName('images')[7].style.marginLeft = '-700px';
-document.getElementsByClassName('imagesBlockHidden')[8].style.marginLeft = '800px'
-document.getElementsByClassName('images')[8].style.marginLeft = '-800px'
-document.getElementsByClassName('imagesBlockHidden')[9].style.marginLeft = '900px'
-document.getElementsByClassName('images')[9].style.marginLeft = '-900px'
-}
-slider();
+      return (rezult[i] =
+        Object.keys(o)[i].replace(/[A-Z]+/, t) + ":" + x + ";");
+    });
+
+    return e.setAttribute("style", rezult.join(""));
+  },
+  imageArray() {
+    return Array.from(this.getId(this.id).getElementsByTagName("img"));
+  },
+  countArray(n) {
+    let a = [];
+    for (let i = 0; i < n; i++) {
+      a[i] = i;
+    }
+    return a;
+  },
+  createEl(e) {
+    return document.body.appendChild(document.createElement(e));
+  },
+  createUl() {
+    this.createEl("ul").setAttribute("id", this.idUl);
+  },
+  imgStyle() {
+    this.imageArray().map((img) => this.css(img, { display: "none" }));
+  },
+  listStyle() {
+    this.getClass(this.classLi).map((li) => this.css(li, this.liStyle));
+  },
+  createLi() {
+    this.countArray(5).map((li) =>
+      this.createEl("li").setAttribute("class", this.classLi)
+    );
+  },
+  createList() {
+    this.getClass(this.classLi).map((li, i) =>
+      this.getId(this.idUl).appendChild(this.getClass(this.classLi)[i])
+    );
+  },
+  display() {
+    this.createUl();
+    this.createLi();
+    this.imgStyle();
+    this.listStyle();
+    this.createList();
+  }
+};
+slider.display();
